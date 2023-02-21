bc2ab480ffa8729c7d9bf728cc7552a6c9fb652c
Washington99 <Raotraotthristanw@gmail.com>
2023-02-21 20:03:53 +0800

    Successful implementation of add model functions
    Can now input album, artist, and song models from the admin panel

--- thrisraotraot_music/homepage/admin.py
+++ thrisraotraot_music/homepage/admin.py
@@ -2,9 +2,17 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Artist
+from .models import Artist, Album, Song
 
 class ArtistAdmin(admin.ModelAdmin):
     model = Artist
+
+class AlbumAdmin(admin.ModelAdmin):
+    model = Album
+    
+class SongAdmin(admin.ModelAdmin):
+    model = Song
     
 admin.site.register(Artist, ArtistAdmin)
+admin.site.register(Album, AlbumAdmin)
+admin.site.register(Song, SongAdmin)
--- thrisraotraot_music/homepage/models.py
+++ thrisraotraot_music/homepage/models.py
@@ -4,16 +4,22 @@ from django.db import models
 
 class Artist(models.Model):
     artist_name = models.CharField(max_length = 50)
+    birth_name = models.CharField(max_length = 50, blank = True)
     monthly_listeners = models.IntegerField()
+    bio = models.TextField(max_length = 700, blank = True)
 
 class Album(models.Model):
     album_name = models.CharField(max_length = 50)
     artist = models.ForeignKey(Artist, on_delete = models.CASCADE)
     description = models.CharField(max_length = 100)
     release_date = models.IntegerField()
+    label = models.CharField(max_length = 50, blank = True)
+    song_count = models.IntegerField(default = "0")
     
 class Song(models.Model):
     song_title = models.CharField(max_length = 50)
     artist = models.ForeignKey(Artist, on_delete = models.CASCADE)
     album = models.ForeignKey(Album, on_delete = models.CASCADE)
     song_length = models.IntegerField()
+    music_video = models.BooleanField(default = False)
+    lyrics = models.TextField(max_length = 1000, blank = True)
