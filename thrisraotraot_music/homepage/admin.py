bebcee499fb862f1238d3c9ecd64befcdfca0d89
Washington99 <Raotraotthristanw@gmail.com>
2023-02-21 18:47:17 +0800

    First Succesful test
    Successfully added an entry into table artist

--- thrisraotraot_music/homepage/admin.py
+++ thrisraotraot_music/homepage/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Artist
+
+class ArtistAdmin(admin.ModelAdmin):
+    model = Artist
+    
+admin.site.register(Artist, ArtistAdmin)
